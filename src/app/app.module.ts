d2540a23b2070b7bcd29d26ecb3fe86567c11ae4
Karim <mail@ackarim.com>
2022-02-10 12:39:15 +0100

    Adding assets, basic styles and brands more bg for mobile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrandsComponent } from './brands/brands.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BrandsComponent
   ],
   imports: [
     BrowserModule
